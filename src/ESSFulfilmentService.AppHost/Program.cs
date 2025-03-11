702109aa5f33d70d93c19bf4a5359557e34171fe
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-03-11 18:14:03 +0000

    Enhance ESSFulfilmentService with Docker support
    Updated project configuration to include Dockerfile and WAR file in output.
    Changed IIC service containerization method to use Dockerfile.
    Modified Worker logic to use "notify" queue and improved message processing.
    Introduced a new Dockerfile for consistent deployment using Tomcat.

--- src/ESSFulfilmentService.AppHost/Program.cs
+++ src/ESSFulfilmentService.AppHost/Program.cs
@@ -9,13 +9,12 @@ var blobstorage = storage.AddBlobs("blobConnection");
 var queuestorage = storage.AddQueues("queueConnection");
 var tablestorage = storage.AddTables("tableConnection");
 
-// This will fail because we have not yet created the container image for the IIC service
-var iic_custom = builder.AddContainer("iic", "richardahz/iic","latest")
+
+var iic_custom = builder.AddDockerfile("iic",".")
     .WithHttpEndpoint(port: 8080, targetPort: 8080, name: "iic-endpoint");
 
 var iic_endpoint = iic_custom.GetEndpoint("iic-endpoint");
 
-//This won't start because we have not yet created the container image for the IIC service
 builder.AddProject<Projects.ESSFulfilmentService_Builder>("essfulfilmentservice-builder")
     .WithReference(blobstorage)
     .WaitFor(blobstorage)
