bc697ddb9f46edd4f6835ccde7b36259a7aab4f5
John Rippington <john@portolan.co>
2025-03-24 08:33:10 +0000

    Removed sample dash, added OTLP/Prometheus/Grafana implementation

---
+++ src/UKHO.ADDS.EFS.LocalHost/OpenTelemetryCollector/OpenTelemetryCollectorServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Aspire.Hosting.Lifecycle;
+
+namespace UKHO.ADDS.EFS.LocalHost.OpenTelemetryCollector;
+
+internal static class OpenTelemetryCollectorServiceExtensions
+{
+    public static IDistributedApplicationBuilder AddOpenTelemetryCollectorInfrastructure(this IDistributedApplicationBuilder builder)
+    {
+        builder.Services.TryAddLifecycleHook<OpenTelemetryCollectorLifecycleHook>();
+
+        return builder;
+    }
+}
