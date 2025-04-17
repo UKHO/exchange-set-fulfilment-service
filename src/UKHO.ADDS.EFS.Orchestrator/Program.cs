31f707c6314db19c63d4fa605485b7ded6fdadbc
Mohammed Khan <mohamme15315@mastek.com>
2025-04-17 14:43:21 +0530

    Added ILogger in program.cs AB#214754

--- src/UKHO.ADDS.EFS.Orchestrator/Program.cs
+++ src/UKHO.ADDS.EFS.Orchestrator/Program.cs
@@ -110,7 +110,7 @@ namespace UKHO.ADDS.EFS.Orchestrator
                 return factory.CreateClient(salesCatalogueEndpoint, "");
             });
 
-            builder.Services.AddSingleton(x => new JobService(salesCatalogueEndpoint, x.GetRequiredService<ExchangeSetJobTable>(), x.GetRequiredService<ExchangeSetTimestampTable>(), x.GetRequiredService<ISalesCatalogueClient>()));
+            builder.Services.AddSingleton(x => new JobService(salesCatalogueEndpoint, x.GetRequiredService<ExchangeSetJobTable>(), x.GetRequiredService<ExchangeSetTimestampTable>(), x.GetRequiredService<ISalesCatalogueClient>(), x.GetRequiredService<ILogger<JobService>>()));
         }
     }
 }
