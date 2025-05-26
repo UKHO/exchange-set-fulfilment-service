f3c9b4f0066b75d29af9cee40f227050f59e9b8f
Vipin Mahajan <vipin14904@mastek.com>
2025-05-26 13:19:41 +0530

    Created common branch

--- src/UKHO.ADDS.EFS.Builder.S100/Pipelines/CreationPipeline.cs
+++ src/UKHO.ADDS.EFS.Builder.S100/Pipelines/CreationPipeline.cs
@@ -1,15 +1,22 @@
-﻿using UKHO.ADDS.EFS.Builder.S100.Pipelines.Create;
+﻿using UKHO.ADDS.EFS.Builder.S100.IIC;
+using UKHO.ADDS.EFS.Builder.S100.Pipelines.Create;
 using UKHO.ADDS.Infrastructure.Pipelines.Nodes;
 
 namespace UKHO.ADDS.EFS.Builder.S100.Pipelines
 {
     internal class CreationPipeline : IBuilderPipeline<ExchangeSetPipelineContext>
     {
+        private readonly IToolClient _toolClient;
+
+        public CreationPipeline(IToolClient toolClient)
+        {
+            _toolClient = toolClient ?? throw new ArgumentNullException(nameof(toolClient));
+        }
+
         public async Task<NodeResult> ExecutePipeline(ExchangeSetPipelineContext context)
         {
             var pipeline = new PipelineNode<ExchangeSetPipelineContext>();
 
-            pipeline.AddChild(new CreateWorkspaceNode());
             pipeline.AddChild(new CreateExchangeSetNode());
 
             var result = await pipeline.ExecuteAsync(context);
