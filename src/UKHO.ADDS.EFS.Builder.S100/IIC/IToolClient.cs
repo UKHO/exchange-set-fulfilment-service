34c87861c9ac4c6e4b75fee12253b733280e3689
pradn1501261 <124887603+Pradnya-Bhosale@users.noreply.github.com>
2025-06-04 12:13:19 +0530

    217380,218893 -EFS Consuming IIC Extract ExchangeSet and FSS Add File Endpoints (#57)
    * Added code changes download file node AB#218131
    
    * added unit test case fine AB#218132
    
    * Upgarde latest packages AB#218131
    
    * Added unit test cases and minor code changes AB#218132
    
    * 216779 - added logging model for batch search and download  node  AB#216779
    
    * added code refactor changes AB#218131
    
    * Fixed review comments AB#223500
    
    * Minor change added AB#223500
    
    * Update subproject commit to latest version
    AB#216779
    
    * Update package versions and access modifiers
    
    - Updated `Microsoft.AspNetCore.OpenApi` package from version `9.0.0` to `9.0.5` in `UKHO.ADDS.Mocks.EFS.csproj`.
    - Changed `DownloadFilesNode` class access modifier from `public` to `internal` in `DownloadFilesNode.cs` for improved encapsulation.
    - Modified `InternalsVisibleTo` attribute in `UKHO.ADDS.EFS.Builder.S100.csproj` to reference `UKHO.ADDS.EFS.Builder.S100.UnitTests` instead of `UKHO.ADDS.EFS.Builder.S100.Tests`.
    
    AB#216779
    
    * Fixed review comments AB#223500
    
    * Added code for upload files AB#218893
    
    * 217380 - added coding for extracting and downloading exchange set AB#217380 AB#223314
    
    * 217380 - unit test code and return stream for exchange set AB#217380 AB#223314 AB#223315
    
    * 217380 - minor code changes in unit test code AB#217380
    
    * Updated logging functionality and using ExchangeSetStream in uploadFilesNodes AB#218893
    
    * One minor change AB#218893
    
    * Added unit test for UploadFilesNode class AB#218893
    
    * 217380 - modified code and UT for destination path  AB#217380
    
    * Updated logs AB#218893
    
    * 217380 - removed logmodel and added folder for exchangeset AB#217380
    
    * 217380 -modified the exchangeset folder name AB#217380
    
    * 217380 - added logger assertion in unit test AB#217380
    
    * Added read file code AB#218893
    
    * 217380 -ut fix for upload files AB#217380 AB#218893
    
    * 217380 - removed modified code  while merging main for DownloadFilesNode AB#217380
    
    * Added testcases for Add files AB#218893
    
    * Asserted logs AB#218893
    
    * 217380 - minor code fixed AB#217380
    
    * 217380 - review comment fixed AB#217380
    
    * Removed inline function AB#218893
    
    * 217380 - resolved review comments
    
    * Revert "217380 - resolved review comments"
    
    This reverts commit 54a446473fa311e38c4ed58c2d755716338b0c93.
    
    * 217380 - resolved review comment #AB217380
    
    * Added review comments AB#218893
    
    * Refactor logging for file operations
    
    Updated event IDs in DistributionPipelineLogging for better clarity on file operation failures. Removed UploadFilesLogView and introduced FileNotFoundLogView for improved logging of missing files.
    AB#218893
    
    * Rename ExchangeSetOutputDirectory to ArchiveFolderName
    AB#218893
    
    * 217380 - resolved review comment AB#217380
    
    ---------
    
    Co-authored-by: Ganpat Gawde <Ganpat14802@Mastek.Com>
    Co-authored-by: Pradnya Bhosale <pradn1501261@mastek.com>
    Co-authored-by: Nadeem Badai <Nadeem14456@Mastek.Com>
    Co-authored-by: Mohammed Khan <mohamme15315@mastek.com>

--- src/UKHO.ADDS.EFS.Builder.S100/IIC/IToolClient.cs
+++ src/UKHO.ADDS.EFS.Builder.S100/IIC/IToolClient.cs
@@ -9,7 +9,7 @@ namespace UKHO.ADDS.EFS.Builder.S100.IIC
         Task<IResult<OperationResponse>> AddExchangeSetAsync(string exchangeSetId, string authKey, string correlationId);
         Task<IResult<OperationResponse>> AddContentAsync(string resourceLocation, string exchangeSetId, string authKey, string correlationId);
         Task<IResult<SigningResponse>> SignExchangeSetAsync(string exchangeSetId, string authKey, string correlationId);
-        Task<IResult<Stream>> ExtractExchangeSetAsync(string exchangeSetId, string authKey, string correlationId);
+        Task<IResult<Stream>> ExtractExchangeSetAsync(string exchangeSetId, string authKey, string correlationId, string destination);
         Task<IResult<string>> ListWorkspaceAsync(string authKey);
     }
 }
---
+++ src/UKHO.ADDS.EFS.Builder.S100/Pipelines/Distribute/Logging/FileNotFoundLogView.cs
@@ -0,0 +1,10 @@
+﻿namespace UKHO.ADDS.EFS.Builder.S100.Pipelines.Distribute.Logging
+{
+    public class FileNotFoundLogView
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string BatchId { get; set; }
+        public string CorrelationId { get; set; }
+    }
+}
