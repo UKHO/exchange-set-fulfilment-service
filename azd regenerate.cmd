f36a10b20835e614fcdcba8d317732510eae64ac
HugoBurgess <128484816+HugoBurgess@users.noreply.github.com>
2025-06-16 16:33:08 +0100

    Add skippable dev deploy stage (#63)
    * Update Snyk checks
    
    * Add azd infra
    
    * IaC scan
    
    * Create bin folder
    
    * Pipeline work
    
    * More pipeline build
    
    * Add transform
    
    * Tidy
    
    * Some rework
    
    * Tidy
    
    * Restore commented out code

---
+++ azd regenerate.cmd
@@ -0,0 +1,20 @@
+@echo off
+title Regenerate
+:: This will regenerate files for deployment (infra, and so on).
+
+set scriptdir=%~dp0
+if not [%scriptdir:~-1%]==[\] set scriptdir=%scriptdir%\
+::echo %scriptdir%
+
+if exist "%scriptdir%azure.yaml" del "%scriptdir%azure.yaml"
+if exist "%scriptdir%infra" rmdir /s /q "%scriptdir%infra"
+if exist "%scriptdir%next-steps.md" del "%scriptdir%next-steps.md"
+if exist "%scriptdir%src\UKHO.ADDS.EFS.LocalHost\infra" rmdir /s /q "%scriptdir%src\UKHO.ADDS.EFS.LocalHost\infra"
+if exist "%scriptdir%.azure" rmdir /s /q "%scriptdir%.azure"
+
+azd init
+azd infra gen
+
+title Regenerate - done
+pause
+exit /b 0
