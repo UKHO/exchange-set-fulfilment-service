317352445eb5cb354690d6d89bd58a8f8c3b6a77
HugoBurgess <hugo.burgess@ukho.gov.uk>
2025-06-24 10:53:16 +0100

    Regen infra

--- src/UKHO.ADDS.EFS.LocalHost/Program.cs
+++ src/UKHO.ADDS.EFS.LocalHost/Program.cs
@@ -73,19 +73,19 @@ namespace UKHO.ADDS.EFS.LocalHost
             var storageTable = storage.AddTables(StorageConfiguration.TablesName);
             var storageBlob = storage.AddBlobs(StorageConfiguration.BlobsName);
 
-            builder.AddDockerfile(ContainerConfiguration.S100BuilderContainerName, "..", "UKHO.ADDS.EFS.Builder.S100/Dockerfile")
-                .PublishAsAzureContainerApp((x, y) =>
-                {
-                    y.Template.Scale.MinReplicas = 0;
-                    y.Template.Scale.MaxReplicas = 1000;
-                    y.Configuration.Ingress = new ContainerAppIngressConfiguration
-                    {
-                        External = false,
-                        TargetPort = 8080,
-                        Transport = new BicepValue<ContainerAppIngressTransportMethod>("http")
-                    };
-                })
-                .WithExternalHttpEndpoints();
+            //builder.AddDockerfile(ContainerConfiguration.S100BuilderContainerName, "..", "UKHO.ADDS.EFS.Builder.S100/Dockerfile")
+            //    .PublishAsAzureContainerApp((x, y) =>
+            //    {
+            //        y.Template.Scale.MinReplicas = 0;
+            //        y.Template.Scale.MaxReplicas = 1000;
+            //        y.Configuration.Ingress = new ContainerAppIngressConfiguration
+            //        {
+            //            External = false,
+            //            TargetPort = 8080,
+            //            Transport = new BicepValue<ContainerAppIngressTransportMethod>("http")
+            //        };
+            //    })
+            //    .WithExternalHttpEndpoints();
 
             // ADDS Mock
             var mockService = builder.AddProject<UKHO_ADDS_Mocks_EFS>(ContainerConfiguration.MockContainerName)
--- src/UKHO.ADDS.EFS.LocalHost/infra/efs-builder-s100/efs-builder-s100.tmpl.bicepparam
+++
@@ -1,7 +0,0 @@
-using './efs-builder-s100.module.bicep'
-
-param efs_builder_s100_containerimage = '{{ .Image }}'
-param efs_cae_outputs_azure_container_apps_environment_default_domain = '{{ .Env.EFS_CAE_AZURE_CONTAINER_APPS_ENVIRONMENT_DEFAULT_DOMAIN }}'
-param efs_cae_outputs_azure_container_apps_environment_id = '{{ .Env.EFS_CAE_AZURE_CONTAINER_APPS_ENVIRONMENT_ID }}'
-param efs_cae_outputs_azure_container_registry_endpoint = '{{ .Env.EFS_CAE_AZURE_CONTAINER_REGISTRY_ENDPOINT }}'
-param efs_cae_outputs_azure_container_registry_managed_identity_id = '{{ .Env.EFS_CAE_AZURE_CONTAINER_REGISTRY_MANAGED_IDENTITY_ID }}'
