00d50b0d62ea4e07933e9333a50edac2d9e18c8f
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-07-23 11:40:15 +0100

    Update to Azure Functions Worker SDK and .NET 9.0
    Refactor `PostJobRequestFunction.cs` to use dependency injection for logging and configuration, enabling HTTP POST requests with a JSON payload.
    
    Update `UKHO.ADDS.EFS.Trigger.csproj` to target .NET 9.0 and include necessary Azure Functions Worker SDK references.
    
    Introduce `Program.cs` to set up the Azure Functions application with service defaults and register `HttpClient` for dependency injection.

--- src/UKHO.ADDS.EFS.Trigger/PostJobRequestFunction.cs
+++ src/UKHO.ADDS.EFS.Trigger/PostJobRequestFunction.cs
@@ -1,18 +1,51 @@
 using System;
-using Microsoft.Azure.WebJobs;
-
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using UKHO.ADDS.EFS.Configuration.Namespaces;
 
 namespace UKHO.ADDS.EFS.Trigger
 {
     public class PostJobRequestFunction
     {
-        [FunctionName("PostJobRequestFunction")]
-        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        private readonly ILogger _logger;
+        private readonly IConfiguration _config;
+        private readonly string _httpsEndpoint;
+        private readonly HttpClient _httpClient;
+        public PostJobRequestFunction(ILoggerFactory loggerFactory, IConfiguration config, IHttpClientFactory httpFactory)
         {
+            _logger = loggerFactory.CreateLogger<PostJobRequestFunction>();
+            _config = config ?? throw new ArgumentNullException(nameof(config));
+            _httpsEndpoint = _config.GetSection($"services:{ProcessNames.OrchestratorService}:https:0").Value;
+            _httpClient = httpFactory.CreateClient();
+            _httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+            _httpClient.BaseAddress = new Uri(_httpsEndpoint);
+        }
+
+        [Function(nameof(PostJobRequestFunction))]
+        public async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo infoTimer)
+        {
+            var content = new StringContent(
+                """
+                {
+                    "version": 1,
+                    "dataStandard": "s100",
+                    "products": "",
+                    "filter": ""
+                }
+                """,
+                Encoding.UTF8, "application/json");
+            var requestId = Guid.NewGuid().ToString();
+            content.Headers.Add("x-correlation-id", $"job-{requestId}");
 
 
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var jobSubmitResponse = await _httpClient.PostAsync("/jobs", content);
+
+            _logger.LogInformation($"Requested S100 exchange set at: {DateTime.Now}");
+            _logger.LogInformation($"Next request schedule at: {infoTimer.ScheduleStatus.Next}");
         }
     }
 }
---
+++ src/UKHO.ADDS.EFS.Trigger/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+namespace UKHO.ADDS.EFS.Trigger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = FunctionsApplication.CreateBuilder(args);
+
+            builder.AddServiceDefaults();
+
+            builder.Services.AddHttpClient();
+
+            builder.Build().Run();
+        }
+    }
+}
