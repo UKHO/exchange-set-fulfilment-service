2143a3509b17d3eadd0ae400481dbbd354a1d2d0
Mohammed Khan <mohamme15315@mastek.com>
2025-08-06 21:55:28 +0530

    Added sample Kiota Changes AB#235201

--- src/UKHO.ADDS.EFS.Orchestrator/InjectionExtensions.cs
+++ src/UKHO.ADDS.EFS.Orchestrator/InjectionExtensions.cs
@@ -1,9 +1,16 @@
-﻿using System.Text.Json;
-using Microsoft.AspNetCore.Http.Json;
+﻿using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+using Microsoft.Kiota.Http.HttpClientLibrary.Middleware;
+using Microsoft.Kiota.Authentication.Azure;
 using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http.Json;
 using UKHO.ADDS.Aspire.Configuration.Remote;
+using UKHO.ADDS.Clients.Common.MiddlewareExtensions;
 using UKHO.ADDS.Clients.FileShareService.ReadWrite;
+using UKHO.ADDS.Clients.Kiota.FileShareService.ReadWrite;
+using UKHO.ADDS.Clients.Kiota.SalesCatalogueService;
 using UKHO.ADDS.Clients.SalesCatalogueService;
 using UKHO.ADDS.EFS.Builds;
 using UKHO.ADDS.EFS.Builds.S100;
@@ -29,114 +36,137 @@ using UKHO.ADDS.EFS.Orchestrator.Services.Infrastructure;
 using UKHO.ADDS.EFS.Orchestrator.Services.Storage;
 using UKHO.ADDS.Infrastructure.Serialization.Json;
 
-namespace UKHO.ADDS.EFS.Orchestrator
+namespace UKHO.ADDS.EFS.Orchestrator;
+
+internal static class InjectionExtensions
 {
-    internal static class InjectionExtensions
+    private const string OpenApiRequiredType = "string";
+
+    public static WebApplicationBuilder AddOrchestratorServices(this WebApplicationBuilder builder)
     {
-        private const string OpenApiRequiredType = "string";
+        var configuration = builder.Configuration;
+        builder.Services.AddHttpContextAccessor();
 
-        public static WebApplicationBuilder AddOrchestratorServices(this WebApplicationBuilder builder)
+        builder.Services.Configure<JsonOptions>(options => JsonCodec.DefaultOptions.CopyTo(options.SerializerOptions));
+
+        builder.AddAzureQueueClient(StorageConfiguration.QueuesName);
+        builder.AddAzureTableClient(StorageConfiguration.TablesName);
+        builder.AddAzureBlobClient(StorageConfiguration.BlobsName);
+
+        builder.Services.AddAuthorization();
+        builder.Services.AddOpenApi();
+
+        builder.Services.ConfigureOpenApi();
+
+        builder.Services.AddTransient<AssemblyPipelineFactory>();
+        builder.Services.AddTransient<AssemblyPipelineNodeFactory>();
+
+        builder.Services.AddTransient<CompletionPipelineFactory>();
+        builder.Services.AddTransient<CompletionPipelineNodeFactory>();
+
+        builder.Services.AddSingleton<PipelineContextFactory<S100Build>>();
+        builder.Services.AddSingleton<PipelineContextFactory<S63Build>>();
+        builder.Services.AddSingleton<PipelineContextFactory<S57Build>>();
+
+        builder.Services.AddHostedService<S100BuildResponseMonitor>();
+        builder.Services.AddHostedService<S63BuildResponseMonitor>();
+        builder.Services.AddHostedService<S57BuildResponseMonitor>();
+
+        builder.Services.AddSingleton<ITimestampService, TimestampService>();
+        builder.Services.AddSingleton<IStorageService, StorageService>();
+        builder.Services.AddSingleton<IHashingService, HashingService>();
+
+        builder.Services.AddSingleton<ITable<S100Build>, S100BuildTable>();
+        builder.Services.AddSingleton<ITable<S63Build>, S63BuildTable>();
+        builder.Services.AddSingleton<ITable<S57Build>, S57BuildTable>();
+
+        builder.Services.AddSingleton<ITable<DataStandardTimestamp>, DataStandardTimestampTable>();
+        builder.Services.AddSingleton<ITable<Job>, JobTable>();
+        builder.Services.AddSingleton<ITable<BuildMemento>, BuildMementoTable>();
+
+        builder.Services.AddSingleton<IBuilderLogForwarder, BuilderLogForwarder>();
+        builder.Services.AddSingleton<StorageInitializerService>();
+
+        // Configure Kiota clients for FSS orchestrator endpoints
+        builder.Services.ConfigureKiotaServices(configuration);
+
+        // Keep existing Sales Catalogue Service registration
+        builder.Services.AddSingleton<ISalesCatalogueClientFactory>(provider => new SalesCatalogueClientFactory(provider.GetRequiredService<IHttpClientFactory>()));
+
+        builder.Services.AddSingleton(provider =>
         {
-            var configuration = builder.Configuration;
-            builder.Services.AddHttpContextAccessor();
+            var factory = provider.GetRequiredService<ISalesCatalogueClientFactory>();
+            var registry = provider.GetRequiredService<IExternalServiceRegistry>();
 
-            builder.Services.Configure<JsonOptions>(options => JsonCodec.DefaultOptions.CopyTo(options.SerializerOptions));
+            var scsEndpoint = registry.GetServiceEndpointAsync(ProcessNames.SalesCatalogueService).GetAwaiter().GetResult();
 
-            builder.AddAzureQueueClient(StorageConfiguration.QueuesName);
-            builder.AddAzureTableClient(StorageConfiguration.TablesName);
-            builder.AddAzureBlobClient(StorageConfiguration.BlobsName);
+            return factory.CreateClient(scsEndpoint.Uri!.ToString(), string.Empty);
+        });
 
-            builder.Services.AddAuthorization();
-            builder.Services.AddOpenApi();
+        builder.Services.AddSingleton<IOrchestratorSalesCatalogueClient, OrchestratorSalesCatalogueClient>();
+        builder.Services.AddSingleton<IOrchestratorFileShareClient, OrchestratorFileShareClient>();
 
-            builder.Services.ConfigureOpenApi();
-
-            builder.Services.AddTransient<AssemblyPipelineFactory>();
-            builder.Services.AddTransient<AssemblyPipelineNodeFactory>();
-
-            builder.Services.AddTransient<CompletionPipelineFactory>();
-            builder.Services.AddTransient<CompletionPipelineNodeFactory>();
-
-            builder.Services.AddSingleton<PipelineContextFactory<S100Build>>();
-            builder.Services.AddSingleton<PipelineContextFactory<S63Build>>();
-            builder.Services.AddSingleton<PipelineContextFactory<S57Build>>();
-
-            builder.Services.AddHostedService<S100BuildResponseMonitor>();
-            builder.Services.AddHostedService<S63BuildResponseMonitor>();
-            builder.Services.AddHostedService<S57BuildResponseMonitor>();
-
-            builder.Services.AddSingleton<ITimestampService, TimestampService>();
-            builder.Services.AddSingleton<IStorageService, StorageService>();
-            builder.Services.AddSingleton<IHashingService, HashingService>();
-
-            builder.Services.AddSingleton<ITable<S100Build>, S100BuildTable>();
-            builder.Services.AddSingleton<ITable<S63Build>, S63BuildTable>();
-            builder.Services.AddSingleton<ITable<S57Build>, S57BuildTable>();
-
-            builder.Services.AddSingleton<ITable<DataStandardTimestamp>, DataStandardTimestampTable>();
-            builder.Services.AddSingleton<ITable<Job>, JobTable>();
-            builder.Services.AddSingleton<ITable<BuildMemento>, BuildMementoTable>();
-
-            builder.Services.AddSingleton<IBuilderLogForwarder, BuilderLogForwarder>();
-            builder.Services.AddSingleton<StorageInitializerService>();
-
-            builder.Services.AddSingleton<ISalesCatalogueClientFactory>(provider => new SalesCatalogueClientFactory(provider.GetRequiredService<IHttpClientFactory>()));
-
-            builder.Services.AddSingleton(provider =>
-            {
-                var factory = provider.GetRequiredService<ISalesCatalogueClientFactory>();
-                var registry = provider.GetRequiredService<IExternalServiceRegistry>();
-
-                var scsEndpoint = registry.GetServiceEndpointAsync(ProcessNames.SalesCatalogueService).GetAwaiter().GetResult();
-
-                return factory.CreateClient(scsEndpoint.Uri!.ToString(), string.Empty);
-            });
-
-            builder.Services.AddSingleton<IFileShareReadWriteClientFactory>(provider => new FileShareReadWriteClientFactory(provider.GetRequiredService<IHttpClientFactory>()));
-
-            builder.Services.AddSingleton(provider =>
-            {
-                var factory = provider.GetRequiredService<IFileShareReadWriteClientFactory>();
-                var registry = provider.GetRequiredService<IExternalServiceRegistry>();
-
-                var fssEndpoint = registry.GetServiceEndpointAsync(ProcessNames.FileShareService).GetAwaiter().GetResult();
-
-                return factory.CreateClient(fssEndpoint.Uri!.ToString(), string.Empty);
-            });
-
-            builder.Services.AddSingleton<IOrchestratorSalesCatalogueClient, OrchestratorSalesCatalogueClient>();
-            builder.Services.AddSingleton<IOrchestratorFileShareClient, OrchestratorFileShareClient>();
-
-            return builder;
-        }
-
-        private static IServiceCollection ConfigureOpenApi(this IServiceCollection serviceCollection)
-        {
-            serviceCollection.AddOpenApi(options =>
-            {
-                options.AddOperationTransformer((operation, context, cancellationToken) =>
-                {
-                    var headers = context.Description.ActionDescriptor.EndpointMetadata.OfType<OpenApiHeaderParameter>();
-
-                    foreach (var header in headers)
-                    {
-                        operation.Parameters ??= new List<OpenApiParameter>();
-
-                        operation.Parameters.Add(new OpenApiParameter
-                        {
-                            Name = header.Name,
-                            In = ParameterLocation.Header,
-                            Required = header.Required,
-                            Description = header.Description,
-                            Schema = new OpenApiSchema { Type = OpenApiRequiredType, Default = new OpenApiString(header.ExpectedValue) }
-                        });
-                    }
-
-                    return Task.CompletedTask;
-                });
-            });
-
-            return serviceCollection;
-        }
+        return builder;
     }
+
+    /// <summary>
+    /// Configures Kiota services for FSS orchestrator endpoints (create, commit, set expiry, search)
+    /// </summary>
+    private static IServiceCollection ConfigureKiotaServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Configure authentication provider - using Anonymous for mock services, Azure Identity for production
+        var authProvider = new AnonymousAuthenticationProvider();
+        // Uncomment the line below to use Azure Identity for authentication in production
+        // var authProvider = new AzureIdentityAuthenticationProvider(new DefaultAzureCredential());
+        
+        // Register Kiota defaults with authentication provider
+        services.AddKiotaDefaults(authProvider);
+
+        // Register FileShare Service Kiota client for FSS orchestrator endpoints using standardized approach
+        services.RegisterKiotaClient<KiotaFileShareServiceReadWrite>("FileShareService:Endpoint");
+
+        return services;
+    }
+
+    private static IServiceCollection ConfigureOpenApi(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddOpenApi(options =>
+        {
+            options.AddOperationTransformer((operation, context, cancellationToken) =>
+            {
+                var headers = context.Description.ActionDescriptor.EndpointMetadata.OfType<OpenApiHeaderParameter>();
+
+                foreach (var header in headers)
+                {
+                    operation.Parameters ??= new List<OpenApiParameter>();
+
+                    operation.Parameters.Add(new OpenApiParameter
+                    {
+                        Name = header.Name,
+                        In = ParameterLocation.Header,
+                        Required = header.Required,
+                        Description = header.Description,
+                        Schema = new OpenApiSchema { Type = OpenApiRequiredType, Default = new OpenApiString(header.ExpectedValue) }
+                    });
+                }
+
+                return Task.CompletedTask;
+            });
+        });
+
+        return serviceCollection;
+    }
+
+    public static IServiceCollection AddKiotaHandlers(this IServiceCollection services)
+    {
+        var kiotaHandlers = KiotaClientFactory.GetDefaultHandlerActivatableTypes();
+        //var kiotaHandlers2 = KiotaClientFactory.GetDefaultHandlerTypes();
+
+        foreach (var handler in kiotaHandlers)
+        {
+            services.AddTransient(handler);
+        }
+        return services;
+    }
+
 }
--- src/UKHO.ADDS.EFS.Orchestrator/Services/Infrastructure/OrchestratorFileShareClient.cs
+++ src/UKHO.ADDS.EFS.Orchestrator/Services/Infrastructure/OrchestratorFileShareClient.cs
@@ -1,9 +1,19 @@
-﻿using UKHO.ADDS.Clients.FileShareService.ReadOnly.Models;
-using UKHO.ADDS.Clients.FileShareService.ReadWrite;
+﻿using Docker.DotNet.Models;
+using Microsoft.Kiota.Abstractions;
+using UKHO.ADDS.Clients.Common.MiddlewareExtensions;
+using UKHO.ADDS.Clients.FileShareService.ReadOnly.Models;
 using UKHO.ADDS.Clients.FileShareService.ReadWrite.Models;
 using UKHO.ADDS.Clients.FileShareService.ReadWrite.Models.Response;
+using UKHO.ADDS.Clients.Kiota.FileShareService.ReadWrite;
+using UKHO.ADDS.Clients.Kiota.FileShareService.ReadWrite;
 using UKHO.ADDS.EFS.Orchestrator.Infrastructure.Logging;
+using UKHO.ADDS.EFS.Orchestrator.Jobs;
 using UKHO.ADDS.Infrastructure.Results;
+using BatchExpiryModel = UKHO.ADDS.Clients.Kiota.FileShareService.ReadWrite;
+using BatchModel = UKHO.ADDS.Clients.Kiota.FileShareService.ReadWrite;
+using Error = UKHO.ADDS.Infrastructure.Results.Error;
+using IError = UKHO.ADDS.Infrastructure.Results.IError;
+using ReadOnlyBatchDetails = UKHO.ADDS.Clients.FileShareService.ReadOnly.Models.BatchDetails;
 
 namespace UKHO.ADDS.EFS.Orchestrator.Services.Infrastructure
 {
@@ -21,7 +31,7 @@ namespace UKHO.ADDS.EFS.Orchestrator.Services.Infrastructure
         private const string CommitBatch = "CommitBatch";
         private const string CreateBatch = "CreateBatch";
         private const string AddFileToBatch = "AddFileToBatch";
-        private readonly IFileShareReadWriteClient _fileShareReadWriteClient;
+        private readonly KiotaFileShareServiceReadWrite _fileShareReadWriteClient;
         private readonly ILogger<OrchestratorFileShareClient> _logger;
 
         /// <summary>
@@ -30,7 +40,7 @@ namespace UKHO.ADDS.EFS.Orchestrator.Services.Infrastructure
         /// <param name="fileShareReadWriteClient">The file share read-write client.</param>
         /// <param name="logger">The logger.</param>
         /// <exception cref="ArgumentNullException">Thrown when fileShareReadWriteClient or logger is null.</exception>
-        public OrchestratorFileShareClient(IFileShareReadWriteClient fileShareReadWriteClient, ILogger<OrchestratorFileShareClient> logger)
+        public OrchestratorFileShareClient(KiotaFileShareServiceReadWrite fileShareReadWriteClient, ILogger<OrchestratorFileShareClient> logger)
         {
             _fileShareReadWriteClient = fileShareReadWriteClient ?? throw new ArgumentNullException(nameof(fileShareReadWriteClient));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
@@ -44,14 +54,19 @@ namespace UKHO.ADDS.EFS.Orchestrator.Services.Infrastructure
         /// <returns>A result containing the batch handle on success or error information on failure.</returns>
         public async Task<IResult<IBatchHandle>> CreateBatchAsync(string correlationId, CancellationToken cancellationToken)
         {
-            var createBatchResponseResult = await _fileShareReadWriteClient.CreateBatchAsync(GetBatchModel(), correlationId, cancellationToken);
 
-            if (createBatchResponseResult.IsFailure(out var error, out _))
+            var createBatchResponse = await _fileShareReadWriteClient.Batch.PostAsync(
+                null,
+                r => r.Headers.Add("X-Correlation-Id", correlationId),
+                cancellationToken);
+
+            if (createBatchResponse == null)
             {
-                LogFileShareServiceError(correlationId, CreateBatch, error, correlationId);
+                return Result.Failure<IBatchHandle>(new Error("CreateBatch returned null response"));
             }
 
-            return createBatchResponseResult;
+            //return Result.Success<IBatchHandle>(new BatchHandle(createBatchResponse.BatchId!));
+            return Result.Success<IBatchHandle>(new BatchHandle(correlationId));
         }
 
         /// <summary>
@@ -63,14 +78,9 @@ namespace UKHO.ADDS.EFS.Orchestrator.Services.Infrastructure
         /// <returns>A result containing the commit batch response on success or error information on failure.</returns>
         public async Task<IResult<CommitBatchResponse>> CommitBatchAsync(string batchId, string correlationId, CancellationToken cancellationToken)
         {
-            var commitBatchResult = await _fileShareReadWriteClient.CommitBatchAsync(new BatchHandle(batchId), correlationId, cancellationToken);
+            //await _fileShareReadWriteClient.V1.Batches[batchId].PostAsync(null, r => r.Options.Add(new CorrelationIdHandlerOption { CorrelationId = correlationId }), cancellationToken);
 
-            if (commitBatchResult.IsFailure(out var commitError, out _))
-            {
-                LogFileShareServiceError(correlationId, CommitBatch, commitError, correlationId, batchId);
-            }
-
-            return commitBatchResult;
+            return Result.Success(new CommitBatchResponse());
         }
 
         /// <summary>
@@ -82,16 +92,34 @@ namespace UKHO.ADDS.EFS.Orchestrator.Services.Infrastructure
         /// <returns>A result containing the batch search response on success or error information on failure.</returns>
         public async Task<IResult<BatchSearchResponse>> SearchCommittedBatchesExcludingCurrentAsync(string currentBatchId, string correlationId, CancellationToken cancellationToken)
         {
-            var filter = $"BusinessUnit eq '{BusinessUnit}' and {ProductTypeQueryClause}$batch(BatchId) ne '{currentBatchId}'";
+            //var filter = $"BusinessUnit eq '{BusinessUnit}' and {ProductTypeQueryClause}$batch(BatchId) ne '{currentBatchId}'";
 
-            var searchResult = await _fileShareReadWriteClient.SearchAsync(filter, Limit, Start, correlationId, cancellationToken);
+            //var searchResult = await _fileShareReadWriteClient.V1.Batches.GetAsync(r =>
+            //{
+            //    r.QueryParameters.Filter = filter;
+            //    r.QueryParameters.Limit = Limit;
+            //    r.QueryParameters.Start = Start;
+            //    r.Options.Add(new CorrelationIdHandlerOption { CorrelationId = correlationId });
+            //}, cancellationToken);
 
-            if (searchResult.IsFailure(out var error, out _))
-            {
-                LogSearchCommittedBatchesError(currentBatchId, correlationId, filter, Limit, Start, error);
-            }
 
-            return searchResult;
+            //if (searchResult == null)
+            //{
+            //    var error = new Error("SearchCommittedBatches returned null response");
+            //    LogSearchCommittedBatchesError(currentBatchId, correlationId, filter, Limit, Start, error);
+            //    return Result.Failure<BatchSearchResponse>(error);
+            //}
+
+            //var response = new BatchSearchResponse
+            //{
+            //    Count = searchResult.Count ?? 0,
+            //    Total = searchResult.Total ?? 0,
+            //    Entries = searchResult.Entries?.Select(b => new ReadOnlyBatchDetails { BatchId = b.BatchId, BusinessUnit = b.BusinessUnit, Status = (BatchStatus)b.Status!, Files = b.Files?.Select(f => new FileDetails { Filename = f.Filename, FileSize = f.FileSize, Hash = f.Hash, MimeType = f.MimeType }).ToList() }).ToList() ?? new List<ReadOnlyBatchDetails>(),
+            //    Links = new Links(new Link(searchResult.Links?.Self?.Href ?? string.Empty))
+            //};
+
+            //return Result.Success(response);
+            return Result.Success(new BatchSearchResponse());
         }
 
         /// <summary>
@@ -104,32 +132,35 @@ namespace UKHO.ADDS.EFS.Orchestrator.Services.Infrastructure
         ///     A result containing the last set expiry date response on success or error information on failure.
         ///     If no valid batches are found, returns a success result with an empty response.
         /// </returns>
-        public async Task<IResult<SetExpiryDateResponse>> SetExpiryDateAsync(List<BatchDetails> otherBatches, string correlationId, CancellationToken cancellationToken)
+        public async Task<IResult<SetExpiryDateResponse>> SetExpiryDateAsync(List<ReadOnlyBatchDetails> otherBatches, string correlationId, CancellationToken cancellationToken)
         {
-            // Filter valid batches before processing
-            var validBatches = otherBatches.Where(b => !string.IsNullOrEmpty(b.BatchId)).ToList();
+            //// Filter valid batches before processing
+            //var validBatches = otherBatches.Where(b => !string.IsNullOrEmpty(b.BatchId)).ToList();
 
-            if (validBatches.Count == 0)
-            {
-                return Result.Success(new SetExpiryDateResponse());
-            }
+            //if (validBatches.Count == 0)
+            //{
+            //    return Result.Success(new SetExpiryDateResponse());
+            //}
 
-            IResult<SetExpiryDateResponse> lastResult = Result.Success(new SetExpiryDateResponse());
+            //IResult<SetExpiryDateResponse> lastResult = Result.Success(new SetExpiryDateResponse());
 
-            foreach (var batch in validBatches)
-            {
-                var expiryResult = await _fileShareReadWriteClient.SetExpiryDateAsync(batch.BatchId, new BatchExpiryModel { ExpiryDate = DateTime.UtcNow }, correlationId, cancellationToken);
+            //foreach (var batch in validBatches)
+            //{
+            //    try
+            //    {
+            //        await _fileShareReadWriteClient.V1.Batches[batch.BatchId!].Expiry.PutAsync(new Clients.FileShareService.ReadWrite.Models.BatchExpiryModel { ExpiryDate = DateTime.UtcNow }, r => r.Options.Add(new CorrelationIdHandlerOption { CorrelationId = correlationId }), cancellationToken);
+            //        lastResult = Result.Success(new SetExpiryDateResponse());
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        var error = new Error(ex.Message);
+            //        LogFileShareServiceError(correlationId, SetExpiryDate, error, correlationId, batch.BatchId);
+            //        return Result.Failure<SetExpiryDateResponse>(error);
+            //    }
+            //}
 
-                if (expiryResult.IsFailure(out var expiryError, out _))
-                {
-                    LogFileShareServiceError(correlationId, SetExpiryDate, expiryError, correlationId, batch.BatchId);
-                    return expiryResult;
-                }
-
-                lastResult = expiryResult;
-            }
-
-            return lastResult;
+            //return lastResult;
+            return Result.Success(new SetExpiryDateResponse());
         }
 
         /// <summary>
@@ -144,22 +175,36 @@ namespace UKHO.ADDS.EFS.Orchestrator.Services.Infrastructure
         /// <returns>A result containing the add file to batch response on success or error information on failure.</returns>
         public async Task<IResult<AddFileToBatchResponse>> AddFileToBatchAsync(string batchId, Stream fileStream, string fileName, string contentType, string correlationId, CancellationToken cancellationToken)
         {
-            var batchHandle = new BatchHandle(batchId);
-            var addFileResult = await _fileShareReadWriteClient.AddFileToBatchAsync(batchHandle, fileStream, fileName, contentType, correlationId, cancellationToken);
-
-            if (addFileResult.IsFailure(out var error, out _))
+            try
             {
-                LogFileShareServiceError(correlationId, AddFileToBatch, error, correlationId, batchId);
-            }
+                //var requestBody = new MemoryStream();
+                //await fileStream.CopyToAsync(requestBody, cancellationToken);
+                //requestBody.Seek(0, SeekOrigin.Begin);
 
-            return addFileResult;
+                //await _fileShareReadWriteClient.V1.Batches[batchId].Files[fileName].PostAsync(requestBody, r =>
+                //{
+                //    r.Headers.Add("Content-Type", contentType);
+                //    r.Options.Add(new CorrelationIdHandlerOption { CorrelationId = correlationId });
+                //}, cancellationToken);
+
+                return Result.Success(new AddFileToBatchResponse());
+            }
+            catch (Exception ex)
+            {
+                var error = new Error(ex.Message);
+                LogFileShareServiceError(correlationId, AddFileToBatch, error, correlationId, batchId);
+                return Result.Failure<AddFileToBatchResponse>(error);
+            }
         }
 
         /// <summary>
         ///     Creates a batch model with predefined settings for S-100 product type.
         /// </summary>
         /// <returns>A configured batch model with appropriate access control and attributes.</returns>
-        private static BatchModel GetBatchModel() =>
+        //private static Clients.FileShareService.ReadWrite.Models.BatchModel GetBatchModel() =>
+        //    new() { BusinessUnit = "ADDS-S100", Acl = new UKHO.ADDS.Clients.Kiota.FileShareService.ReadWrite.V1.Models.Acl { ReadUsers = new List<string> { "public" }, ReadGroups = new List<string> { "public" } }, Attributes = new List<KeyValuePair<string, string>> { new("Exchange Set Type", "Base"), new("Frequency", "DAILY"), new("Product Type", "S-100"), new("Media Type", "Zip") }, ExpiryDate = null };
+
+        private static Clients.FileShareService.ReadWrite.Models.BatchModel GetBatchModel() =>
             new() { BusinessUnit = "ADDS-S100", Acl = new Acl { ReadUsers = new List<string> { "public" }, ReadGroups = new List<string> { "public" } }, Attributes = new List<KeyValuePair<string, string>> { new("Exchange Set Type", "Base"), new("Frequency", "DAILY"), new("Product Type", "S-100"), new("Media Type", "Zip") }, ExpiryDate = null };
 
         private void LogFileShareServiceError(string jobId, string endPoint, IError error, string correlationId, string batchId = "")
