db2dcb005cad0250953330b2b8cb51d5c7bbaa57
HugoBurgess <hugo.burgess@ukho.gov.uk>
2025-08-06 12:46:56 +0100

    Add service identity

--- src/UKHO.ADDS.EFS.LocalHost/Program.cs
+++ src/UKHO.ADDS.EFS.LocalHost/Program.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using Aspire.Hosting.Azure;
 using Azure.Provisioning.AppConfiguration;
 using Azure.Provisioning.AppContainers;
 using Azure.Provisioning.Storage;
@@ -45,6 +46,12 @@ namespace UKHO.ADDS.EFS.LocalHost
             // Get parameters
             var subnetResourceId = builder.AddParameter("subnetResourceId");
             var zoneRedundant = builder.AddParameter("zoneRedundant");
+            var efsServiceIdentityName = builder.AddParameter("efsServiceIdentityName");
+            var efsServiceIdentityResourceGroup = builder.AddParameter("efsServiceIdentityResourceGroup");
+
+            // Existing user managed identity for SCS/FSS
+            var efsServiceIdentity = builder.AddAzureUserAssignedIdentity(ServiceConfiguration.EfsServiceIdentity)
+                .PublishAsExisting(efsServiceIdentityName, efsServiceIdentityResourceGroup);
 
             // Container apps environment
             var acaEnv = builder.AddAzureContainerAppEnvironment(ServiceConfiguration.AcaEnvironmentName)
