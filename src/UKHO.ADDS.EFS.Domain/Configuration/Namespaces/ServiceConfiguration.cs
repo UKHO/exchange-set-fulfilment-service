7f97a240cfb2d5afdb75e9e669a139504a97a2fa
HugoBurgess <hugo.burgess@ukho.gov.uk>
2025-08-07 09:19:36 +0100

    Reinstate pipeline

--- src/UKHO.ADDS.EFS.Domain/Configuration/Namespaces/ServiceConfiguration.cs
+++ src/UKHO.ADDS.EFS.Domain/Configuration/Namespaces/ServiceConfiguration.cs
@@ -5,6 +5,5 @@
         public const string ServiceName = "EFS";
         public const string AcaEnvironmentName = "efs-cae";
         public const string EfsServiceIdentity = "efs-service-identity";
-        public const string EfsResourceIdentity = "efs-resource-identity";
     }
 }
