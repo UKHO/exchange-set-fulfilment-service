da1f9064d046d4b691c520849423a0360b7c58ee
Pradnya Bhosale <pradn1501261@mastek.com>
2025-08-28 12:55:40 +0530

    235200 - modified the connectionString

--- src/UKHO.ADDS.EFS.Orchestrator/Program.cs
+++ src/UKHO.ADDS.EFS.Orchestrator/Program.cs
@@ -50,7 +50,7 @@ namespace UKHO.ADDS.EFS.Orchestrator
                 //}
                 //else
                 //{
-                var connectionString = builder.Configuration.GetConnectionString(ServiceConfiguration.EventHubName);//Environment.GetEnvironmentVariable("ConnectionStrings__efs-events-namespace");
+                var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__efs-events-namespace");
                 var eventHubName = ServiceConfiguration.EventHubName;
 
                 builder.Services.AddSerilog((services, lc) =>
