fe50588af5e0a67b8904208303014ecc2e8d1cfc
Chaitali Shidid <Chaital14503@Mastek.Com>
2025-09-04 19:01:59 +0530

    235205 - Review comments resolved AB#235205

--- src/UKHO.ADDS.EFS.Orchestrator/Health/FileShareServiceHealthCheck.cs
+++ src/UKHO.ADDS.EFS.Orchestrator/Health/FileShareServiceHealthCheck.cs
@@ -1,27 +1,28 @@
 using UKHO.ADDS.Aspire.Configuration.Remote;
 using UKHO.ADDS.EFS.Infrastructure.Configuration.Namespaces;
 
-namespace UKHO.ADDS.EFS.Orchestrator.Health;
-
-/// <summary>
-/// Health check for File Share Service connectivity
-/// </summary>
-internal class FileShareServiceHealthCheck : BaseServiceHealthCheck
+namespace UKHO.ADDS.EFS.Orchestrator.Health
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="FileShareServiceHealthCheck"/> class.
+    /// Health check for File Share Service connectivity
     /// </summary>
-    /// <param name="httpClientFactory">HTTP client factory for creating HTTP clients.</param>
-    /// <param name="externalServiceRegistry">Registry for retrieving service endpoint information.</param>
-    /// <param name="logger">Logger for recording diagnostic information.</param>
-    public FileShareServiceHealthCheck(
-        IHttpClientFactory httpClientFactory,
-        IExternalServiceRegistry externalServiceRegistry,
-        ILogger<FileShareServiceHealthCheck> logger)
-        : base(httpClientFactory, externalServiceRegistry, logger)
+    internal class FileShareServiceHealthCheck : BaseServiceHealthCheck
     {
-    }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileShareServiceHealthCheck"/> class.
+        /// </summary>
+        /// <param name="httpClientFactory">HTTP client factory for creating HTTP clients.</param>
+        /// <param name="externalServiceRegistry">Registry for retrieving service endpoint information.</param>
+        /// <param name="logger">Logger for recording diagnostic information.</param>
+        public FileShareServiceHealthCheck(
+            IHttpClientFactory httpClientFactory,
+            IExternalServiceRegistry externalServiceRegistry,
+            ILogger<FileShareServiceHealthCheck> logger)
+            : base(httpClientFactory, externalServiceRegistry, logger)
+        {
+        }
 
-    protected override string ServiceName => "File Share Service";
-    protected override string ProcessName => ProcessNames.FileShareService;
+        protected override string ServiceName => "File Share Service";
+        protected override string ProcessName => ProcessNames.FileShareService;
+    }
 }
