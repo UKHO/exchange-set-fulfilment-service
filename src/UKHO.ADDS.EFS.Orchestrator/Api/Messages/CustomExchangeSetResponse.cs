8527c7ea803809fb48a7ce7d14ee2a8b89c5194d
John Rippington <john.rippington@ukho.gov.uk>
2025-09-05 13:29:19 +0100

    Cherry picked from PR #142, #126. (#156)
    * Cherry picked from PR #142, #126. Tests from #109 to be reviewed separately, code changes incorporated where needed.
    
    * Added ProductCount to output, updated use of BatchId
    
    * Updated tests

---
+++ src/UKHO.ADDS.EFS.Orchestrator/Api/Messages/CustomExchangeSetResponse.cs
@@ -0,0 +1,47 @@
+﻿using UKHO.ADDS.EFS.Domain.Jobs;
+using UKHO.ADDS.EFS.Domain.Products;
+using UKHO.ADDS.EFS.Orchestrator.Api.Models;
+
+namespace UKHO.ADDS.EFS.Orchestrator.Api.Messages
+{
+    /// <summary>
+    /// Response model for exchange set endpoints
+    /// </summary>
+    internal class CustomExchangeSetResponse
+    {
+        /// <summary>
+        /// Links to related resources
+        /// </summary>
+        public required ExchangeSetLinks Links { get; set; }
+
+        /// <summary>
+        /// The expiry date and time for the exchange set URL
+        /// </summary>
+        public required DateTime ExchangeSetUrlExpiryDateTime { get; set; }
+
+        /// <summary>
+        /// Number of products explicitly requested
+        /// </summary>
+        public ProductCount RequestedProductCount { get; set; }
+
+        /// <summary>
+        /// Number of products that have data included in the produced Exchange Set
+        /// </summary>
+        public ProductCount ExchangeSetProductCount { get; set; }
+
+        /// <summary>
+        /// Number of requested products that are already up to date
+        /// </summary>
+        public ProductCount RequestedProductsAlreadyUpToDateCount { get; set; }
+
+        /// <summary>
+        /// Products that were requested but not included in the exchange set
+        /// </summary>
+        public MissingProductList RequestedProductsNotInExchangeSet { get; set; } = new();
+
+        /// <summary>
+        /// The FSS Batch ID associated with the exchange set
+        /// </summary>
+        public BatchId FssBatchId { get; set; }
+    }
+}
