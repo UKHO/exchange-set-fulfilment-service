800e7f8f9593d8f1e0a529ff2e57cce65d18de56
pradn1501261 <124887603+Pradnya-Bhosale@users.noreply.github.com>
2025-09-10 10:58:32 +0530

    235200 - EFS Event Hub Enhancement (#154)
    * 235200 - added event hub changes and fix correlationid in log AB#235200
    
    * 235200 - resolved merge conflict AB#235200
    
    * 235200 - environment value issue resolved AB#235200
    
    * 235200 - added for environment value AB#235200
    
    * 235200 - changes for environment name AB#235200
    
    * 235200 -commented options to check logs AB#235200
    
    * 235200 - removed duplicate properties appeared in logs AB#235200
    
    * 235200 - resolved the snyk issue AB#235200
    
    * 235200 - resolve review comment AB#235200
    
    * 235200 - resolved the sonar qube issues AB#235200
    
    * 235200 - unit test fix AB#235200
    
    * 235200 - removed event hub unit test code AB#235200
    
    * 235200 - commented blob storage code for event hub logging AB#235200
    
    * 235200 - remove blob storage code for event hub logging AB#235200
    
    * 235200 - resolved review comment AB#235200
    
    ---------
    
    Co-authored-by: Pradnya Bhosale <pradn1501261@mastek.com>

--- src/UKHO.ADDS.EFS.Orchestrator/Schedule/SchedulerJob.cs
+++ src/UKHO.ADDS.EFS.Orchestrator/Schedule/SchedulerJob.cs
@@ -1,4 +1,6 @@
 ﻿using Quartz;
+using Serilog.Context;
+using UKHO.ADDS.EFS.Domain.Constants;
 using UKHO.ADDS.EFS.Domain.External;
 using UKHO.ADDS.EFS.Domain.Messages;
 using UKHO.ADDS.EFS.Domain.Products;
@@ -33,11 +35,14 @@ namespace UKHO.ADDS.EFS.Orchestrator.Schedule
         /// <returns>Task representing the asynchronous job execution.</returns>
         public async Task Execute(IJobExecutionContext context)
         {
-            try
-            {
-                var correlationId = CorrelationId.From($"{CorrelationIdPrefix}{Guid.NewGuid():N}");
+            var correlationId = CorrelationId.From($"{CorrelationIdPrefix}{Guid.NewGuid():N}");
 
-                _logger.LogSchedulerJobStarted(correlationId, DateTime.UtcNow);
+            // Properly push correlation ID to Serilog LogContext for the entire request
+            using (LogContext.PushProperty(LogProperties.CorrelationId, correlationId))
+            {
+                try
+                {
+                    _logger.LogSchedulerJobStarted(correlationId, DateTime.UtcNow);
 
                 var message = new JobRequestApiMessage
                 {
@@ -46,20 +51,22 @@ namespace UKHO.ADDS.EFS.Orchestrator.Schedule
                     Filter = ""
                 };
 
-                var parameters = AssemblyPipelineParameters.CreateFrom(message, _config, correlationId);
-                var pipeline = _pipelineFactory.CreateAssemblyPipeline(parameters);
+                    var parameters = AssemblyPipelineParameters.CreateFrom(message, _config, correlationId);
+                    var pipeline = _pipelineFactory.CreateAssemblyPipeline(parameters);
 
-                var result = await pipeline.RunAsync(CancellationToken.None);
+                    var result = await pipeline.RunAsync(CancellationToken.None);
 
-                _logger.LogSchedulerJobCompleted(correlationId, result);
+                    _logger.LogSchedulerJobCompleted(correlationId, result);
 
-                _logger.LogSchedulerJobNextRun(context.Trigger.GetNextFireTimeUtc()?.DateTime);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogSchedulerJobException(ex);
-                throw;
+                    _logger.LogSchedulerJobNextRun(context.Trigger.GetNextFireTimeUtc()?.DateTime);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogSchedulerJobException(ex);
+                    throw;
+                }
             }
         }
+
     }
 }
