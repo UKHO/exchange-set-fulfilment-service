e92ebbded66929f63bcf5bff474657779a3f5a8d
Sheetal Dalavi <sheetal15692@mastek.com>
2025-09-12 18:40:21 +0530

    AB#244063 - Enhance product name tests and add submission method
    Updated `ProductNamesFunctionalTests.cs` to include new test cases for validating product names and callback URIs, removing the constructor and adding scenarios for empty arrays and invalid URIs. Introduced tests for S-101, S-102, S-104, and S-111 products, as well as multiple product requests.
    
    Added `ProductNamesInCustomAssemblyPipelineSubmitJobAsync` method in `OrchestratorCommands.cs` to submit jobs to the product names endpoint and validate response counts. Imported necessary namespaces for the new functionality.

--- test/UKHO.ADDS.EFS.FunctionalTests/ProductNamesFunctionalTests.cs
+++ test/UKHO.ADDS.EFS.FunctionalTests/ProductNamesFunctionalTests.cs
@@ -6,26 +6,43 @@ namespace UKHO.ADDS.EFS.FunctionalTests
 {
     public class ProductNamesFunctionalTests : TestBase
     {
-        private readonly ITestOutputHelper _output;
-
-        public ProductNamesFunctionalTests(ITestOutputHelper output)
-        {
-            _output = output;
-        }
 
         //PBI 242670 - Input validation for the ESS API - Product Name Endpoint
         [Theory]
-        [InlineData(new object[] { "101GB40079ABCDEFG", "102NO32904820801012", "104US00_CHES_TYPE1_20210630_0600", "111US00_ches_dcf8_20190703T00Z" }, "https://valid.com/callback", HttpStatusCode.Accepted, "")] // Test Case 243519 - Valid input
+        [InlineData(new object[] { "101GB40079ABCDEFG", "102NO32904820801012"}, "", HttpStatusCode.Accepted, "")] // Test Case 243519 - Valid input with valid callBackURI
         [InlineData(new object[] { "112GB40079ABCDEFG" }, "https://valid.com/callback", HttpStatusCode.BadRequest, "112GB40079ABCDEFG' starts with digits '112' but that is not a valid S-100 product")] // Test Case 245717 -Invalid Product
         [InlineData(new object[] { }, "https://valid.com/callback", HttpStatusCode.BadRequest, "ProductName cannot be null or empty.")] // Test Case 243604 - Empty array
         [InlineData(new object[] { "" }, "https://valid.com/callback", HttpStatusCode.BadRequest, "ProductName cannot be null or empty.")] //Test Case 243605 - Array with Empty string
-        [InlineData(new object[] { "101GB40079ABCDEFG" }, "InvalidCallbackUri", HttpStatusCode.BadRequest, "Invalid callbackUri format.")] // Test Case 245020 - Invalid CallBackUrl
-        [InlineData(new object[] { "101GB40079ABCDEFG", 123, 456, 789 }, "https://valid.com/callback", HttpStatusCode.InternalServerError, "")] //Test Case 243659 - Mixed valid and invalid data types
+        [InlineData(new object[] { "101GB40079ABCDEFG" }, "InvalidCallbackUri", HttpStatusCode.BadRequest, "Please enter a valid callback URI in HTTPS format.")] // Test Case 245020 - Invalid CallBackUrl
+        [InlineData(new object[] { "101GB40079ABCDEFG", 123, 456, 789 }, "https://valid.com/callback", HttpStatusCode.BadRequest, "Either body is null or malformed.")] //Test Case 243659 - Mixed valid and invalid data types
         public async Task ValidateProductNamesEndpoint(object[] productNames, string? callbackUri, HttpStatusCode expectedStatusCode, string expectedErrorMessage)
         {
             var httpClient = App!.CreateHttpClient(ProcessNames.OrchestratorService);
 
             await OrchestratorCommands.VerifyProductNamesEndpointResponse(productNames, httpClient, callbackUri, expectedStatusCode, expectedErrorMessage);
+        }
+
+        //PBI 244063 - Use the existing Product Names Node (GetS100ProductNamesNode) from existing pipeline (S100AssemblyPipeline) to new pipeline (S100CustomAssemblyPipeline).
+        [Theory]
+        [InlineData(new object[] { "101GB004DEVQK" }, "https://valid.com/callback", "Single101Product.zip")] // Test Case 245610 - Product Name (S-101 product) Node Integration
+        [InlineData(new object[] { "102CA005N5040W00130" }, "https://valid.com/callback", "Single102Product.zip")] // Test Case 245610 - Product Name (S-102 product) Node Integration
+        [InlineData(new object[] { "104CA00_20241103T001500Z_GB3DEVK0_DCF2" }, "https://valid.com/callback", "Single104Product.zip")] // Test Case 245610 - Product Name (S-104 product) Node Integration
+        [InlineData(new object[] { "111FR00_20241217T001500Z_GB3DEVK0_DCF2" }, "https://valid.com/callback", "Single111Product.zip")] // Test Case 245610 - Product Name (S-111 product) Node Integration
+        [InlineData(new object[] { "111CA00_20241217T001500Z_GB3DEVQ0_DCF2", "104CA00_20241103T001500Z_GB3DEVK0_DCF2" }, "https://valid.com/callback", "MultipleProducts.zip")]   // Test Case 245610 - Product Names (multiple products) Node Integration
+        public async Task ValidateProductNamesNodeInCustomAssemblyPipeline(object[] productNames, string? callbackUri, string zipFileName)
+        {
+            var httpClient = App!.CreateHttpClient(ProcessNames.OrchestratorService);
+
+            var jobId = await OrchestratorCommands.ProductNamesInCustomAssemblyPipelineSubmitJobAsync(httpClient, callbackUri, productNames);
+
+            await OrchestratorCommands.WaitForJobCompletionAsync(httpClient, jobId);
+
+            await OrchestratorCommands.VerifyBuildStatusAsync(httpClient, jobId);
+
+            var exchangeSetDownloadPath = await ZipStructureComparer.DownloadExchangeSetAsZipAsync(jobId, App!);
+            var sourceZipPath = Path.Combine(ProjectDirectory!, "TestData", zipFileName);
+
+            ZipStructureComparer.CompareZipFilesExactMatch(sourceZipPath, exchangeSetDownloadPath);
         }
     }
 }
