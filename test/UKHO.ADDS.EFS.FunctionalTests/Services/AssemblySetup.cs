1281f74921f1628968b0cd226e88b0fd8bdb057a
Amit Dattatray Dalvi <amit.dalvi@mastek.com>
2025-09-17 18:57:31 +0530

    PBI-247488 TASK-247676 Configure assembly-level test initialization with AssemblySetup

---
+++ test/UKHO.ADDS.EFS.FunctionalTests/Services/AssemblySetup.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: CollectionBehavior(DisableTestParallelization = false, MaxParallelThreads = 0)]
+[assembly: TestFramework("Meziantou.Xunit.ParallelTestFramework", "Meziantou.Xunit.ParallelTestFramework")]
+
+namespace UKHO.ADDS.EFS.FunctionalTests.Services
+{
+    [CollectionDefinition("Startup")]
+    public class StartupCollection : ICollectionFixture<StartupFixture> { }
+
+    public class StartupFixture : IAsyncLifetime
+    {
+        private AspireResourceSingleton? _singleton;
+
+        public async Task InitializeAsync()
+        {
+            // Initialize the singleton once for all tests
+            _singleton = await AspireResourceSingleton.Instance;
+        }
+
+        public async Task DisposeAsync()
+        {
+            if (_singleton != null)
+            {
+                await _singleton.DisposeAsync();
+            }
+        }
+    }
+}
