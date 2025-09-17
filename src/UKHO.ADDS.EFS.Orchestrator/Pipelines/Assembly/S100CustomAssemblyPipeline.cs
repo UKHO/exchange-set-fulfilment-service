ba0edea6e57cfa41d36906c7aa33be62d3eaafca
Ganpat Gawde <Ganpat14802@Mastek.Com>
2025-09-17 19:48:27 +0530

    Refactor response handling and enhance assembly pipeline
    Updated `S100CustomExchangeSetApiRouteBuilderExtension.cs` to return `Results.Ok` instead of `Results.Accepted`.
    Added new using directives and modified the constructor of `S100CustomAssemblyPipeline` to include parameters for external service registry, configuration, and file name generation.
    Refactored `CreateResponseFromContext` to leverage these services for dynamic endpoint generation and file name creation, improving flexibility and functionality.

--- src/UKHO.ADDS.EFS.Orchestrator/Pipelines/Assembly/S100CustomAssemblyPipeline.cs
+++ src/UKHO.ADDS.EFS.Orchestrator/Pipelines/Assembly/S100CustomAssemblyPipeline.cs
@@ -1,5 +1,8 @@
-﻿using UKHO.ADDS.EFS.Domain.Builds.S100;
+﻿using UKHO.ADDS.Aspire.Configuration.Remote;
+using UKHO.ADDS.EFS.Domain.Builds.S100;
 using UKHO.ADDS.EFS.Domain.Files;
+using UKHO.ADDS.EFS.Domain.Services;
+using UKHO.ADDS.EFS.Infrastructure.Configuration.Namespaces;
 using UKHO.ADDS.EFS.Orchestrator.Api.Messages;
 using UKHO.ADDS.EFS.Orchestrator.Api.Models;
 using UKHO.ADDS.EFS.Orchestrator.Pipelines.Assembly.Nodes.S100;
@@ -10,9 +13,23 @@ namespace UKHO.ADDS.EFS.Orchestrator.Pipelines.Assembly
 {
     internal class S100CustomAssemblyPipeline : AssemblyPipeline<S100Build>
     {
-        public S100CustomAssemblyPipeline(AssemblyPipelineParameters parameters, IAssemblyPipelineNodeFactory nodeFactory, IPipelineContextFactory<S100Build> contextFactory, ILogger<S100CustomAssemblyPipeline> logger)
-    : base(parameters, nodeFactory, contextFactory, logger)
+        private readonly IExternalServiceRegistry _externalServiceRegistry;
+        private readonly IConfiguration _configuration;
+        private readonly IFileNameGeneratorService _fileNameGeneratorService;
+
+        public S100CustomAssemblyPipeline(
+            AssemblyPipelineParameters parameters, 
+            IAssemblyPipelineNodeFactory nodeFactory, 
+            IPipelineContextFactory<S100Build> contextFactory, 
+            ILogger<S100CustomAssemblyPipeline> logger,
+            IExternalServiceRegistry externalServiceRegistry,
+            IConfiguration configuration,
+            IFileNameGeneratorService fileNameGeneratorService)
+            : base(parameters, nodeFactory, contextFactory, logger)
         {
+            _externalServiceRegistry = externalServiceRegistry;
+            _configuration = configuration;
+            _fileNameGeneratorService = fileNameGeneratorService;
         }
 
         public override async Task<AssemblyPipelineResponse> RunAsync(CancellationToken cancellationToken)
@@ -47,19 +64,23 @@ namespace UKHO.ADDS.EFS.Orchestrator.Pipelines.Assembly
             return await ContextFactory.CreatePipelineContext(Parameters);
         }
 
-        private static CustomExchangeSetResponse CreateResponseFromContext(
+        private CustomExchangeSetResponse CreateResponseFromContext(
             PipelineContext<S100Build> context)
         {
-            // Dummy links
+            var fssEndpoint = _externalServiceRegistry.GetServiceEndpoint(ProcessNames.FileShareService);
+            var baseUri = fssEndpoint.Uri.ToString().TrimEnd('/');
+            
+            // Get the exchange set name template from configuration
+            var exchangeSetNameTemplate = _configuration["orchestrator:Builders:S100:ExchangeSetNameTemplate"]!;
+            var fileName = _fileNameGeneratorService.GenerateFileName(exchangeSetNameTemplate, context.Job.Id);
+
+            // Create links using the endpoint from the registry
             var links = new ExchangeSetLinks
             {
-                ExchangeSetBatchStatusUri = new Link { Uri = new Uri($"http://fss.ukho.gov.uk/batch/{context.Job.BatchId}/status") },
-                ExchangeSetBatchDetailsUri = new Link { Uri = new Uri($"http://fss.ukho.gov.uk/batch/{context.Job.BatchId}") },
-                ExchangeSetFileUri = new Link { Uri = new Uri($"http://fss.ukho.gov.uk/batch/{context.Job.BatchId}/files/exchangeset.zip") }
+                ExchangeSetBatchStatusUri = new Link { Uri = new Uri($"{baseUri}/batch/{context.Job.BatchId}/status") },
+                ExchangeSetBatchDetailsUri = new Link { Uri = new Uri($"{baseUri}/batch/{context.Job.BatchId}") },
+                ExchangeSetFileUri = new Link { Uri = new Uri($"{baseUri}/batch/{context.Job.BatchId}/files/{fileName}") }
             };
-
-            // Dummy missing products list
-            var missingProducts = context.Job.RequestedProductsNotInExchangeSet;
 
             return new CustomExchangeSetResponse
             {
