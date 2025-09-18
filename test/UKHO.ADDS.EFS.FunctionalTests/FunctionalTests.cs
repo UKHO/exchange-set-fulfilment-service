1819e611a3a9e1bad79814339dc3d781db6ae878
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-09-18 13:12:11 +0100

    Suspend some tests

--- test/UKHO.ADDS.EFS.FunctionalTests/FunctionalTests.cs
+++ test/UKHO.ADDS.EFS.FunctionalTests/FunctionalTests.cs
@@ -15,26 +15,28 @@ namespace UKHO.ADDS.EFS.FunctionalTests
 
         [Theory]
         [InlineData("ProductName eq '101GB004DEVQK'", "Single101Product.zip")]
-        [InlineData("ProductName eq '102CA005N5040W00130'", "Single102Product.zip")]
-        [InlineData("ProductName eq '104CA00_20241103T001500Z_GB3DEVK0_DCF2'", "Single104Product.zip")]
-        [InlineData("ProductName eq '111FR00_20241217T001500Z_GB3DEVK0_DCF2'", "Single111Product.zip")]
-        [InlineData("ProductName eq '111CA00_20241217T001500Z_GB3DEVQ0_DCF2' or ProductName eq '104CA00_20241103T001500Z_GB3DEVK0_DCF2'", "MultipleProducts.zip")]
-        [InlineData("startswith(ProductName, '101')", "StartWithS101Products.zip")]
-        [InlineData("startswith(ProductName, '102')", "StartWithS102Products.zip")]
-        [InlineData("startswith(ProductName, '104')", "StartWithS104Products.zip")]
-        [InlineData("startswith(ProductName, '111')", "StartWithS111Products.zip")]
-        [InlineData("ProductName eq '101GB004DEVQK' or startswith(ProductName, '104')", "SingleProductAndStartWithS104Products.zip")]
-        [InlineData("startswith(ProductName , '111') or startswith(ProductName,'101')", "StartWithS101AndS111.zip")]
-        [InlineData("startswith(ProductName, '101') or startswith(ProductName, '102') or startswith(ProductName, '104') or startswith(ProductName, '111')", "AllProducts.zip")]
-        [InlineData("startswith(ProductName, '111') or startswith(ProductName, '121')", "StartWithS111Products.zip")]
-        [InlineData("", "WithoutFilter.zip")]
+        //[InlineData("ProductName eq '102CA005N5040W00130'", "Single102Product.zip")]
+        //[InlineData("ProductName eq '104CA00_20241103T001500Z_GB3DEVK0_DCF2'", "Single104Product.zip")]
+        //[InlineData("ProductName eq '111FR00_20241217T001500Z_GB3DEVK0_DCF2'", "Single111Product.zip")]
+        //[InlineData("ProductName eq '111CA00_20241217T001500Z_GB3DEVQ0_DCF2' or ProductName eq '104CA00_20241103T001500Z_GB3DEVK0_DCF2'", "MultipleProducts.zip")]
+        //[InlineData("startswith(ProductName, '101')", "StartWithS101Products.zip")]
+        //[InlineData("startswith(ProductName, '102')", "StartWithS102Products.zip")]
+        //[InlineData("startswith(ProductName, '104')", "StartWithS104Products.zip")]
+        //[InlineData("startswith(ProductName, '111')", "StartWithS111Products.zip")]
+        //[InlineData("ProductName eq '101GB004DEVQK' or startswith(ProductName, '104')", "SingleProductAndStartWithS104Products.zip")]
+        //[InlineData("startswith(ProductName , '111') or startswith(ProductName,'101')", "StartWithS101AndS111.zip")]
+        //[InlineData("startswith(ProductName, '101') or startswith(ProductName, '102') or startswith(ProductName, '104') or startswith(ProductName, '111')", "AllProducts.zip")]
+        //[InlineData("startswith(ProductName, '111') or startswith(ProductName, '121')", "StartWithS111Products.zip")]
+        //[InlineData("", "WithoutFilter.zip")]
         public async Task S100FilterTests(string filter, string zipFileName)
         {
             var jobId = await OrchestratorCommands.SubmitJobAsync(httpClient, filter);
+            _output.WriteLine($"Submitted job with ID: {jobId} for filter: {filter}");
 
             await OrchestratorCommands.WaitForJobCompletionAsync(httpClient, jobId);
 
             await OrchestratorCommands.VerifyBuildStatusAsync(httpClient, jobId);
+
             //rhz: add a delay for testing purpose
             await Task.Delay(1000);
             _output.WriteLine($"Job {jobId} completed successfully.");
@@ -48,7 +50,7 @@ namespace UKHO.ADDS.EFS.FunctionalTests
         //Negative scenarios
         [Theory]
         [InlineData("startswith(ProductName, '121')")]
-        [InlineData("ProductName eq '131GB004DEVQK'")]
+        //[InlineData("ProductName eq '131GB004DEVQK'")]
         public async Task S100FilterTestsWithInvalidIdentifier(string filter)
         {
             await OrchestratorCommands.SubmitJobAsync(httpClient, filter, expectedJobStatus: "upToDate", expectedBuildStatus: "none");
