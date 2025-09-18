0b43c5c06bf389a2558ea89baccfde1b9d734c83
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-09-18 12:28:01 +0100

    Fix typo

--- test/UKHO.ADDS.EFS.FunctionalTests/ProductNamesFunctionalTests.cs
+++ test/UKHO.ADDS.EFS.FunctionalTests/ProductNamesFunctionalTests.cs
@@ -6,7 +6,7 @@ namespace UKHO.ADDS.EFS.FunctionalTests
     public class ProductNamesFunctionalTests : TestBase
     {
         //PBI 242670 - Input validation for the ESS API - Product Name Endpoint
-        [Theory(Skip = "Temporarily disable"))]
+        [Theory(Skip = "Temporarily disable")]
         [InlineData(new object[] { "101GB40079ABCDEFG", "102NO32904820801012", "104US00_CHES_TYPE1_20210630_0600", "111US00_ches_dcf8_20190703T00Z" }, "https://valid.com/callback", HttpStatusCode.Accepted, "")] // Test Case 243519 - Valid input
         [InlineData(new object[] { "101GB40079ABCDEFG", "102NO32904820801012" }, "", HttpStatusCode.Accepted, "")] // Test Case 243519 - Valid input with valid callBackURI
         [InlineData(new object[] { "112GB40079ABCDEFG" }, "https://valid.com/callback", HttpStatusCode.BadRequest, "112GB40079ABCDEFG' starts with digits '112' but that is not a valid S-100 product")] // Test Case 245717 -Invalid Product
