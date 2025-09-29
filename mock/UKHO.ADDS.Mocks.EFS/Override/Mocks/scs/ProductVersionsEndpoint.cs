c0fd176b38d01b4d235e0b9ac4c1ce3e3d5626df
Ganpat Gawde <Ganpat14802@Mastek.Com>
2025-09-29 19:43:29 +0530

    Update largeexchangesets description for file sizes
    Revised the description for the "get-largeexchangesets" state to clarify that the file sizes are now specified as being more than 300MB, changing from the previous range of 6-10MB.

--- mock/UKHO.ADDS.Mocks.EFS/Override/Mocks/scs/ProductVersionsEndpoint.cs
+++ mock/UKHO.ADDS.Mocks.EFS/Override/Mocks/scs/ProductVersionsEndpoint.cs
@@ -72,7 +72,7 @@ namespace UKHO.ADDS.Mocks.Configuration.Mocks.scs
                     ("Try out the get-cancelledproducts state!", "The response mimics a situation where one of the requested products is cancelled. The final item in the request is marked as cancelled and cancellation details are added in the response with file size 0."),
                     ("Try out the get-productwithdrawn state!", "The response mimics a situation where one of the requested products is withdrawn. The final item in the request is omitted from the returned list and is instead flagged as 'withdrawn', along with a reason like 'productWithdrawn'."),
                     ("Try out the get-productalreadytuptodate state!", "The response mimics a situation where one of the requested products is up to date. The final item in the request is omitted from the returned list and count is shown in requestedProductsAlreadyUpToDateCount property."),
-                    ("Try out the get-largeexchangesets state!", "The response mimics a situation where all products have large file sizes (6-10MB). This simulates the scenario where the exchange set will be large in size.")
+                    ("Try out the get-largeexchangesets state!", "The response mimics a situation where all products have large file sizes (more than 300MB). This simulates the scenario where the exchange set will be large in size.")
                 ));
     }
 }
