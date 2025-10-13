7e9a041f5e9764bd94cbded6140529143748bedb
Avinash15515 <Avinash15515@IN-MH1LPW112665>
2025-10-13 20:35:44 +0530

    Restrict access to FileBlockStorage class and members
    Changed the `FileBlockStorage` class and its `FileBlocks`
    dictionary from `public` to `internal` to limit their
    accessibility to within the same assembly, improving
    encapsulation and adhering to the principle of least
    privilege.

--- mock/UKHO.ADDS.Mocks.EFS/Override/Mocks/fss/Models/FileBlockStorage.cs
+++ mock/UKHO.ADDS.Mocks.EFS/Override/Mocks/fss/Models/FileBlockStorage.cs
@@ -3,9 +3,9 @@ namespace UKHO.ADDS.Mocks.EFS.Override.Mocks.fss.Models
     /// <summary>
     /// Static class to store file blocks in memory, shared between upload and write endpoints
     /// </summary>
-    public static class FileBlockStorage
+    internal static class FileBlockStorage
     {
         // Dictionary structure: { "batchId:fileName": { "blockId": blockData } }
-        public static readonly Dictionary<string, Dictionary<string, byte[]>> FileBlocks = new();
+        internal static readonly Dictionary<string, Dictionary<string, byte[]>> FileBlocks = new();
     }
 }
