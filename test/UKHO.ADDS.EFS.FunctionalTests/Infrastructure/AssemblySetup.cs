ae96061a7e49b0af6d19509af5afbdff796a10ea
Amit Dattatray Dalvi <amit.dalvi@mastek.com>
2025-10-14 16:11:03 +0530

    PBI-249580  currently dissabled parallel test runs due to test failing on dev pipeline this can be reverted once pipeline issue resolved

--- test/UKHO.ADDS.EFS.FunctionalTests/Infrastructure/AssemblySetup.cs
+++ test/UKHO.ADDS.EFS.FunctionalTests/Infrastructure/AssemblySetup.cs
@@ -1,10 +1,11 @@
 ﻿using Meziantou.Xunit;
 
-[assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, DisableTestParallelization = false, MaxParallelThreads = -1)]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, DisableTestParallelization = true, MaxParallelThreads = 1)]
 
 namespace UKHO.ADDS.EFS.FunctionalTests.Infrastructure
 {
-    [EnableParallelization] // This enables the parallel execution of classes in a collection
+    // currently dissabled parallel test runs due to test failing on dev pipeline
+    // [EnableParallelization] // This enables the parallel execution of classes in a collection
     [CollectionDefinition("Startup Collection")]
     public class StartupCollection : ICollectionFixture<StartupFixture> { }
 }
