f3355b0459e77ddb593b9b0a3358364786604f12
Amit Dattatray Dalvi <amit.dalvi@mastek.com>
2025-10-29 15:01:03 +0530

    Revert "FT updated under UpdateSinceFunctionalTests.cs for sequential execution"
    This reverts commit acca181d7b67f7f65e5b80e0d1d6bac94c04b04b.

--- test/UKHO.ADDS.EFS.FunctionalTests/Scenarios/UpdateSinceFunctionalTests.cs
+++ test/UKHO.ADDS.EFS.FunctionalTests/Scenarios/UpdateSinceFunctionalTests.cs
@@ -12,7 +12,7 @@ namespace UKHO.ADDS.EFS.FunctionalTests.Scenarios
 {
     [Collection("Startup Collection")]
     [EnableParallelization] // Needed to parallelize inside the class, not just between classes
-    public class UpdateSinceFunctionalTestsA(StartupFixture startup, ITestOutputHelper output) : FunctionalTestBase(startup, output)
+    public class UpdateSinceFunctionalTests(StartupFixture startup, ITestOutputHelper output) : FunctionalTestBase(startup, output)
     {
         private readonly string _requestId = $"job-updateSinceAutoTest-" + Guid.NewGuid();
         private string _endpoint = TestEndpointConfiguration.UpdatesSinceEndpoint;
@@ -26,6 +26,47 @@ namespace UKHO.ADDS.EFS.FunctionalTests.Scenarios
                 callbackUri,
                 productIdentifier,
                 out _assertCallbackTextFile);
+        }
+
+        //PBI 246464 - Consume ESS API - Update Since Endpoint
+        //PBI 242767 - Input validation for the ESS API - Update Since Endpoint
+        [Theory]
+        [DisableParallelization] // This test runs in parallel with other tests. However, its test cases are run sequentially.
+        [InlineData("https://valid.com/callback", "s101", HttpStatusCode.Accepted, "s101UpdateSinceProduct.zip", 0, 0, 1, new [] { "101GB40079ABCDEFG" })] // Test Case 244582 - Valid Format and Test Case 247827 - Valid Format
+        [InlineData("", "s102", HttpStatusCode.Accepted, "s102UpdateSinceProduct.zip", 0, 0, 1, new [] { "102NO32904820801012" })] // Test Case 244585 -  Valid input with blank CallBack Uri
+        [InlineData(null, "s104", HttpStatusCode.Accepted, "s104UpdateSinceProduct.zip", 0, 0, 1, new [] { "104US00_CHES_TYPE1_20210630_0600" })] // Valid input with null CallBack Uri
+        [InlineData("https://valid.com/callback", "s111", HttpStatusCode.Accepted, "s111UpdateSinceProduct.zip", -15, 0, 1, new [] { "111US00_CHES_DCF8_20190703T00Z" })] // Test Case 245730 - Past Date less than 28 days
+        public async Task ValidateUSWithValidPayloadHavingProductIdentifier(string? callbackUri, string? productIdentifier, HttpStatusCode expectedStatusCode, string zipFileName, int days, int expectedRequestedProductCount, int expectedExchangeSetProductCount, string[] productNames)
+        {
+            using var scope = new AssertionScope(); // root scope
+
+            var requestPayload = $"{{ \"sinceDateTime\": \"{DateTime.UtcNow.AddDays(days):yyyy-MM-ddTHH:mm:ss.fffZ}\" }}";
+
+            SetEndpoint(callbackUri, productIdentifier);
+
+            TestOutput.WriteLine($"RequestId: {_requestId}\nRequest EndPoint: {_endpoint}\nRequest Payload: {requestPayload}\nExpectedStatusCode: {expectedStatusCode}");
+
+            await TestExecutionHelper.ExecuteCustomExchangeSetTestSteps(_requestId, requestPayload, _endpoint, zipFileName, expectedRequestedProductCount, expectedExchangeSetProductCount, _assertCallbackTextFile, productNames);
+        }
+
+        [Theory]
+        [InlineData("https://valid.com/callback", "", HttpStatusCode.Accepted, "UpdateSince.zip", 0, 0, 4)] // Valid input with blank Product Identifier
+        [InlineData("", "", HttpStatusCode.Accepted, "UpdateSince.zip", 0, 0, 4)] // Valid input with blank CallBack Uri and Product Identifier
+        [InlineData("https://valid.com/callback", null, HttpStatusCode.Accepted, "UpdateSince.zip", 0, 0, 4)] // Valid input with null Product Identifier
+        [InlineData(null, null, HttpStatusCode.Accepted, "UpdateSince.zip", 0, 0, 4)] // Valid input with null CallBack Uri and Product Identifier
+        public async Task ValidateUSWithValidPayloadNotHavingProductIdentifier(string? callbackUri, string? productIdentifier, HttpStatusCode expectedStatusCode, string zipFileName, int days, int expectedRequestedProductCount, int expectedExchangeSetProductCount)
+        {
+            using var scope = new AssertionScope(); // root scope
+
+            var requestPayload = $"{{ \"sinceDateTime\": \"{DateTime.UtcNow.AddDays(days):yyyy-MM-ddTHH:mm:ss.fffZ}\" }}";
+
+            SetEndpoint(callbackUri, productIdentifier);
+
+            TestOutput.WriteLine($"RequestId: {_requestId}\nRequest EndPoint: {_endpoint}\nRequest Payload: {requestPayload}\nExpectedStatusCode: {expectedStatusCode}");
+
+            var productNames = new[] { "101GB40079ABCDEFG", "102NO32904820801012", "104US00_CHES_TYPE1_20210630_0600", "111US00_CHES_DCF8_20190703T00Z" };
+
+            await TestExecutionHelper.ExecuteCustomExchangeSetTestSteps(_requestId, requestPayload, _endpoint, zipFileName, expectedRequestedProductCount, expectedExchangeSetProductCount, _assertCallbackTextFile, productNames);
         }
 
         [Theory]
@@ -98,64 +139,4 @@ namespace UKHO.ADDS.EFS.FunctionalTests.Scenarios
             await ExchangeSetApiAssertions.CustomExchangeSetSubmitPostRequestAndCheckResponse(_requestId, requestPayload, _endpoint, expectedStatusCode, expectedErrorMessage);
         }
     }
-
-    [Collection("Startup Collection")]
-    public class UpdateSinceFunctionalTestsB(StartupFixture startup, ITestOutputHelper output) : FunctionalTestBase(startup, output)
-    {
-        private readonly string _requestId = $"job-updateSinceAutoTest-" + Guid.NewGuid();
-        private string _endpoint = TestEndpointConfiguration.UpdatesSinceEndpoint;
-        private bool _assertCallbackTextFile = false;
-
-
-        private void SetEndpoint(string? callbackUri, string? productIdentifier)
-        {
-            _endpoint = EndpointUtility.BuildEndpoint(
-                TestEndpointConfiguration.UpdatesSinceEndpoint,
-                callbackUri,
-                productIdentifier,
-                out _assertCallbackTextFile);
-        }
-
-        //PBI 246464 - Consume ESS API - Update Since Endpoint
-        //PBI 242767 - Input validation for the ESS API - Update Since Endpoint
-        [Theory]
-        [DisableParallelization] // This test runs in parallel with other tests. However, its test cases are run sequentially.
-        [InlineData("https://valid.com/callback", "s101", HttpStatusCode.Accepted, "s101UpdateSinceProduct.zip", 0, 0, 1, new[] { "101GB40079ABCDEFG" })] // Test Case 244582 - Valid Format and Test Case 247827 - Valid Format
-        [InlineData("", "s102", HttpStatusCode.Accepted, "s102UpdateSinceProduct.zip", 0, 0, 1, new[] { "102NO32904820801012" })] // Test Case 244585 -  Valid input with blank CallBack Uri
-        [InlineData(null, "s104", HttpStatusCode.Accepted, "s104UpdateSinceProduct.zip", 0, 0, 1, new[] { "104US00_CHES_TYPE1_20210630_0600" })] // Valid input with null CallBack Uri
-        [InlineData("https://valid.com/callback", "s111", HttpStatusCode.Accepted, "s111UpdateSinceProduct.zip", -15, 0, 1, new[] { "111US00_CHES_DCF8_20190703T00Z" })] // Test Case 245730 - Past Date less than 28 days
-        public async Task ValidateUSWithValidPayloadHavingProductIdentifier(string? callbackUri, string? productIdentifier, HttpStatusCode expectedStatusCode, string zipFileName, int days, int expectedRequestedProductCount, int expectedExchangeSetProductCount, string[] productNames)
-        {
-            using var scope = new AssertionScope(); // root scope
-
-            var requestPayload = $"{{ \"sinceDateTime\": \"{DateTime.UtcNow.AddDays(days):yyyy-MM-ddTHH:mm:ss.fffZ}\" }}";
-
-            SetEndpoint(callbackUri, productIdentifier);
-
-            TestOutput.WriteLine($"RequestId: {_requestId}\nRequest EndPoint: {_endpoint}\nRequest Payload: {requestPayload}\nExpectedStatusCode: {expectedStatusCode}");
-
-            await TestExecutionHelper.ExecuteCustomExchangeSetTestSteps(_requestId, requestPayload, _endpoint, zipFileName, expectedRequestedProductCount, expectedExchangeSetProductCount, _assertCallbackTextFile, productNames);
-        }
-
-        [Theory]
-        [InlineData("https://valid.com/callback", "", HttpStatusCode.Accepted, "UpdateSince.zip", 0, 0, 4)] // Valid input with blank Product Identifier
-        [InlineData("", "", HttpStatusCode.Accepted, "UpdateSince.zip", 0, 0, 4)] // Valid input with blank CallBack Uri and Product Identifier
-        [InlineData("https://valid.com/callback", null, HttpStatusCode.Accepted, "UpdateSince.zip", 0, 0, 4)] // Valid input with null Product Identifier
-        [InlineData(null, null, HttpStatusCode.Accepted, "UpdateSince.zip", 0, 0, 4)] // Valid input with null CallBack Uri and Product Identifier
-        public async Task ValidateUSWithValidPayloadNotHavingProductIdentifier(string? callbackUri, string? productIdentifier, HttpStatusCode expectedStatusCode, string zipFileName, int days, int expectedRequestedProductCount, int expectedExchangeSetProductCount)
-        {
-            using var scope = new AssertionScope(); // root scope
-
-            var requestPayload = $"{{ \"sinceDateTime\": \"{DateTime.UtcNow.AddDays(days):yyyy-MM-ddTHH:mm:ss.fffZ}\" }}";
-
-            SetEndpoint(callbackUri, productIdentifier);
-
-            TestOutput.WriteLine($"RequestId: {_requestId}\nRequest EndPoint: {_endpoint}\nRequest Payload: {requestPayload}\nExpectedStatusCode: {expectedStatusCode}");
-
-            var productNames = new[] { "101GB40079ABCDEFG", "102NO32904820801012", "104US00_CHES_TYPE1_20210630_0600", "111US00_CHES_DCF8_20190703T00Z" };
-
-            await TestExecutionHelper.ExecuteCustomExchangeSetTestSteps(_requestId, requestPayload, _endpoint, zipFileName, expectedRequestedProductCount, expectedExchangeSetProductCount, _assertCallbackTextFile, productNames);
-        }
-    }
-
 }
