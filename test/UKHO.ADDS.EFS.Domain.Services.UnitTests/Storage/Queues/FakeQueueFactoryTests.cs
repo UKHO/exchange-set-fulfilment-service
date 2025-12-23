81c57a93ccd2df11cc3af477d14dcc5f22de56a6
hukho <128484816+hukho@users.noreply.github.com>
2025-12-23 16:52:42 +0000

    UKHO.ADDS.EFS.Domain.Services.UnitTests

--- test/UKHO.ADDS.EFS.Domain.Services.UnitTests/Storage/Queues/FakeQueueFactoryTests.cs
+++ test/UKHO.ADDS.EFS.Domain.Services.UnitTests/Storage/Queues/FakeQueueFactoryTests.cs
@@ -3,73 +3,57 @@ using Xunit;
 
 namespace UKHO.ADDS.EFS.Domain.Services.UnitTests.Storage.Queues
 {
-    public class FakeQueueFactoryTests
+    public partial class FakeQueueFactoryTests
     {
-        private static readonly Regex NameRegex = new("^(?=.{3,63}$)(?!-)(?!.*--)[a-z0-9-]+(?<!-)$", RegexOptions.Compiled);
-
-        public static IEnumerable<object[]> ValidNames => new[]
-        {
-            new object[]
-            {
+        public static IEnumerable<object[]> ValidNames =>
+        [
+            [
                 "abc"
-            },
-            new object[]
-            {
+            ],
+            [
                 "queue-name-1"
-            },
-            new object[]
-            {
+            ],
+            [
                 "a23"
-            },
-            new object[]
-            {
+            ],
+            [
                 "a" + new string('b', 61)
-            } // 62
-        };
+            ]
+        ];
 
-        public static IEnumerable<object[]> InvalidNames => new[]
-        {
-            new object?[]
-            {
-                null
-            },
-            new object[]
-            {
+        public static IEnumerable<object[]> InvalidNames =>
+        [
+            [
+                null!
+            ],
+            [
                 string.Empty
-            },
-            new object[]
-            {
+            ],
+            [
                 " "
-            },
-            new object[]
-            {
+            ],
+            [
                 "Abc"
-            },
-            new object[]
-            {
+            ],
+            [
                 "ab_"
-            },
-            new object[]
-            {
+            ],
+            [
                 "-abc"
-            },
-            new object[]
-            {
+            ],
+            [
                 "abc-"
-            },
-            new object[]
-            {
+            ],
+            [
                 "a--b"
-            },
-            new object[]
-            {
+            ],
+            [
                 "ab"
-            },
-            new object[]
-            {
+            ],
+            [
                 "a" + new string('b', 63)
-            } // 64
-        };
+            ]
+        ];
 
         [Theory]
         [MemberData(nameof(ValidNames))]
@@ -78,7 +62,7 @@ namespace UKHO.ADDS.EFS.Domain.Services.UnitTests.Storage.Queues
             var f = new FakeQueueFactory();
             var q = f.GetQueue(name);
             Assert.NotNull(q);
-            Assert.Matches(NameRegex, name);
+            Assert.Matches(NameRegex(), name);
         }
 
         [Theory]
@@ -88,5 +72,8 @@ namespace UKHO.ADDS.EFS.Domain.Services.UnitTests.Storage.Queues
             var f = new FakeQueueFactory();
             Assert.Throws<ArgumentException>(() => f.GetQueue(name!));
         }
+
+        [GeneratedRegex("^(?=.{3,63}$)(?!-)(?!.*--)[a-z0-9-]+(?<!-)$", RegexOptions.Compiled)]
+        private static partial Regex NameRegex();
     }
 }
