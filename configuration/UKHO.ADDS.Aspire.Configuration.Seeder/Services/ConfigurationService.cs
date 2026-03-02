f4ad9811e728792629c5e7e1cf14a52839b53cbb
hukho <128484816+hukho@users.noreply.github.com>
2026-03-02 16:55:36 +0000

    Remove some content types

--- configuration/UKHO.ADDS.Aspire.Configuration.Seeder/Services/ConfigurationService.cs
+++ configuration/UKHO.ADDS.Aspire.Configuration.Seeder/Services/ConfigurationService.cs
@@ -11,7 +11,8 @@ namespace UKHO.ADDS.Aspire.Configuration.Seeder.Services
         {
             var label = serviceName.ToLowerInvariant();
 
-            var configurationSetting = new ConfigurationSetting(WellKnownConfigurationName.ReloadSentinelKey, "change this value to reload all", label) { ContentType = MediaTypeNames.Text.Plain };
+            //var configurationSetting = new ConfigurationSetting(WellKnownConfigurationName.ReloadSentinelKey, "change this value to reload all", label) { ContentType = MediaTypeNames.Text.Plain };
+            var configurationSetting = new ConfigurationSetting(WellKnownConfigurationName.ReloadSentinelKey, "change this value to reload all", label) { ContentType = null };
             await configurationClient.SetConfigurationSettingAsync(configurationSetting, false, cancellationToken);
 
             var configJson = await File.ReadAllTextAsync(configFilePath, cancellationToken);
@@ -21,7 +22,8 @@ namespace UKHO.ADDS.Aspire.Configuration.Seeder.Services
 
             foreach (var value in flattenedConfig)
             {
-                configurationSetting = new ConfigurationSetting(value.Key, value.Value.ConfigValue, label) { ContentType = value.Value.ContentType ?? MediaTypeNames.Text.Plain };
+                //configurationSetting = new ConfigurationSetting(value.Key, value.Value.ConfigValue, label) { ContentType = value.Value.ContentType ?? MediaTypeNames.Text.Plain };
+                configurationSetting = new ConfigurationSetting(value.Key, value.Value.ConfigValue, label) { ContentType = value.Value.ContentType };
                 await configurationClient.SetConfigurationSettingAsync(configurationSetting, false, cancellationToken);
             }
 
@@ -35,7 +37,8 @@ namespace UKHO.ADDS.Aspire.Configuration.Seeder.Services
                 var json = JsonCodec.Encode(externalService);
                 var key = $"{WellKnownConfigurationName.ExternalServiceKeyPrefix}:{externalService.Service}";
 
-                configurationSetting = new ConfigurationSetting(key, json, label) { ContentType = MediaTypeNames.Application.Json };
+                //configurationSetting = new ConfigurationSetting(key, json, label) { ContentType = MediaTypeNames.Application.Json };
+                configurationSetting = new ConfigurationSetting(key, json, label) { ContentType = null };
                 await configurationClient.SetConfigurationSettingAsync(configurationSetting, false, cancellationToken);
             }
         }
