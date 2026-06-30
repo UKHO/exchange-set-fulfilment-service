184eca601787d9822a4d830fceb18667bf4740fb
hukho <128484816+hukho@users.noreply.github.com>
2026-06-30 10:00:43 +0100

    Test data

--- test/UKHO.ADDS.EFS.FunctionalTests/Assertions/ZipArchiveAssertions.cs
+++ test/UKHO.ADDS.EFS.FunctionalTests/Assertions/ZipArchiveAssertions.cs
@@ -29,7 +29,7 @@ namespace UKHO.ADDS.EFS.FunctionalTests.Assertions
 
             // Get distinct directory paths from source archive
             var sourceDirectories = sourceArchive.Entries
-                .Where(x => x.Length > 0)
+                //.Where(x => x.Length > 0)
                 .Select(e => GetDirectoryPath(e.FullName))
                 .Distinct()
                 .OrderBy(e => e)
@@ -39,7 +39,7 @@ namespace UKHO.ADDS.EFS.FunctionalTests.Assertions
 
             // Get distinct directory paths from target archive
             var targetDirectories = targetArchive.Entries
-                .Where(x => x.Length > 0)
+                //.Where(x => x.Length > 0)
                 .Select(e => GetDirectoryPath(e.FullName))
                 .Distinct()
                 .OrderBy(e => e)
